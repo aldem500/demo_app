f5d4f9d65beedd27665c5288c493961c329ab8ba
Alex Demenschonok <aldem@500px.com>
2014-06-16 17:08:35 -0400

    added microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616201445) do
+ActiveRecord::Schema.define(version: 20140616210018) do
+
+  create_table "microposts", id: false, force: true do |t|
+    t.string   "content"
+    t.string   "user"
+    t.integer  "id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
